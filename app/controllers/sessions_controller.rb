c8e80781a152d1b8f9a9127eeee04c3a46118537
tukemen <kawarashun@gmail.com>
2020-08-20 19:08:53 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def new
+  
   end
 
   def create
@@ -9,7 +10,7 @@ class SessionsController < ApplicationController
       #ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in @user
        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      redirect_back_or @user #フレンドリーフォワーディング
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
